5b06d04d426f450f9ec4d2c3767ef693d9fec40d
Heechul Kim <jijisa@iorchard.co.kr>
2023-09-13 13:09:04 +0900

    add slack transport

--- pkgdir/diskpatrol/cli.py
+++ pkgdir/diskpatrol/cli.py
@@ -12,23 +12,27 @@ import argparse
 import requests
 
 from subprocess import Popen
-
 from dotenv import dotenv_values
+
+MSGDIR = "/tmp"
+PROGNAME = 'diskpatrol'
 
 d_level = { "debug": 10, "info": 20, "warning": 30, 
             "error": 40, "critical": 50 }
-MSGDIR = "/tmp"
+
+def clean_tmp():
+    try:
+        logging.debug(f"clean up diskpatrol temp files")
+        for f in glob.glob(f"{MSGDIR}/{PROGNAME}-*"):
+            os.unlink(f)
+            logging.debug(f"deleted {f}")
+    except Exception as e:
+        logging.exception(e)
 
 def handler(signum, frame):
     if signum == signal.SIGINT:
         res = input("Ctrl-C is pressed. Do you want to exit? (y/n) ")
         if res == 'y':
-            logging.debug(f"clean up diskpatrol temp files")
-            try:
-                for f in glob.glob(f"{MSGDIR}/diskpatrol-*"):
-                    os.unlink(f)
-            except Exception as e:
-                logging.exception(e)
             sys.exit(0)
     if signum == signal.SIGHUP:
         logging.info("Received SIGHUP. Reload config file.")
@@ -38,12 +42,22 @@ signal.signal(signal.SIGINT, handler)
 signal.signal(signal.SIGHUP, handler)
 
 def send_telegram(msg: str, token: str, chatid: str) -> None:
+    """Send a Telegram message to a chat id."""
     s_url = f"https://api.telegram.org/bot{token}/sendMessage"
     logging.debug(s_url)
     try:
         response = requests.post(s_url, json={'chat_id': chatid, 'text': msg})
         logging.debug(response.text)
         logging.debug("Sent telegram alert.")
+    except Exception as e:
+        logging.error(e)
+
+def send_slack(msg: str, webhook: str) -> requests.models.Response:
+    """Send a Slack message to a channel via a webhook."""
+    try:
+        response = requests.post(webhook, json={'text': msg})
+        logging.debug(response.text)
+        logging.debug("Sent slack alert.")
     except Exception as e:
         logging.error(e)
 
@@ -54,18 +68,12 @@ def humanize(num, suffix="B"):
         num /= 1024.0
     return f"{num:.1f}Yi{suffix}"
 
-def app(d_vars: dict, parser, config: dict) -> None:
+def app(d_vars: dict, config: dict) -> None:
     """diskpatrol application"""
-    s_prog = parser.prog
     s_fqdn = socket.getaddrinfo(socket.gethostname(), 0, 
             flags=socket.AI_CANONNAME)[0][3]
     s_sender = f'diskpatrol@{s_fqdn}'
     l_recipients = config['RECIPIENT_EMAILS'].split(',')
-
-    if not os.path.isfile(d_vars['conffile']):
-        s_msg = f"Error: cannot find the env file {d_vars['conffile']}\n"
-        logging.error(s_msg)
-        parser.exit(1, s_msg)
 
     d_thresholds = {
         'warning': int(config['WARNING']),
@@ -75,7 +83,7 @@ def app(d_vars: dict, parser, config: dict) -> None:
     
     while True:
         for s_path in config['PATHS'].split(','):
-            s_msgfile_prefix = MSGDIR + '/' + s_prog
+            s_msgfile_prefix = MSGDIR + '/' + PROGNAME
             if os.path.basename(s_path):
                 s_msgfile_prefix += s_path.replace('/', '-')
             else:
@@ -120,16 +128,16 @@ def app(d_vars: dict, parser, config: dict) -> None:
                             i_emailcount = int(f.read())
                     else:
                         # remove other level count files when level is changed.
-                        logging.debug(f"Alert level is changed tp {s_level}")
+                        logging.debug(f"Alert level is changed to {s_level}")
                         logging.debug("Remove other level count files.")
                         if s_level == 'critical':
                             l_countfile = [
-                                f"{s_msgfile_prefix}_warn_emailcount",
+                                f"{s_msgfile_prefix}_warning_emailcount",
                                 f"{s_msgfile_prefix}_error_emailcount"
                             ]
                         elif s_level == 'error':
                             l_countfile = [
-                                f"{s_msgfile_prefix}_warn_emailcount",
+                                f"{s_msgfile_prefix}_warning_emailcount",
                                 f"{s_msgfile_prefix}_critical_emailcount"
                             ]
                         else:
@@ -151,9 +159,9 @@ def app(d_vars: dict, parser, config: dict) -> None:
                         with open(s_emailcount, 'w') as f:
                             f.write('1')
                         i_emailcount = 1
-                    logging.debug(f"Email alert {i_emailcount} " + \
-                                    f"out of {config['EMAIL_ALERT_COUNT']}")
                     if i_emailcount <= int(config['EMAIL_ALERT_COUNT']):
+                        logging.debug(f"Email alert {i_emailcount} " + \
+                                    f"out of {config['EMAIL_ALERT_COUNT']}")
                         logging.debug("Send email alert.")
                         s_mail = f"From: {s_sender}\n" + \
                                 f"To: {config['RECIPIENT_EMAILS']}\n" + \
@@ -162,6 +170,8 @@ def app(d_vars: dict, parser, config: dict) -> None:
                         with smtplib.SMTP(host=config['SMTP_SERVER'],
                                 port=int(config['SMTP_PORT'])) as smtp:
                             smtp.sendmail(s_sender, l_recipients, s_mail)
+                    else:
+                        logging.debug("Skipping Email alert.")
                     with open(s_emailcount, 'w') as f:
                         f.write(str(i_emailcount+1))
                 if config['ENABLE_TELEGRAM'] == '1' and \
@@ -173,16 +183,16 @@ def app(d_vars: dict, parser, config: dict) -> None:
                             i_telegramcount = int(f.read())
                     else:
                         # remove other level count files when level is changed.
-                        logging.debug(f"Alert level is changed tp {s_level}")
+                        logging.debug(f"Alert level is changed to {s_level}")
                         logging.debug("Remove other level count files.")
                         if s_level == 'critical':
                             l_countfile = [
-                                f"{s_msgfile_prefix}_warn_telegramcount",
+                                f"{s_msgfile_prefix}_warning_telegramcount",
                                 f"{s_msgfile_prefix}_error_telegramcount"
                             ]
                         elif s_level == 'error':
                             l_countfile = [
-                                f"{s_msgfile_prefix}_warn_telegramcount",
+                                f"{s_msgfile_prefix}_warning_telegramcount",
                                 f"{s_msgfile_prefix}_critical_telegramcount"
                             ]
                         else:
@@ -204,9 +214,9 @@ def app(d_vars: dict, parser, config: dict) -> None:
                         with open(s_telegramcount, 'w') as f:
                             f.write('1')
                         i_telegramcount = 1
-                    logging.debug(f"Telegram alert: {i_telegramcount} " + \
-                                  f"out of {config['TELEGRAM_ALERT_COUNT']}")
                     if i_telegramcount <= int(config['TELEGRAM_ALERT_COUNT']):
+                        logging.debug(f"Telegram alert: {i_telegramcount} " + \
+                                  f"out of {config['TELEGRAM_ALERT_COUNT']}")
                         logging.debug("Send telegram alert.")
                         if config['APITOKEN'] and config['CHATID']:
                             s_telegram = f"{s_sender}\n{s_subject}\n\n{s_msg}"
@@ -214,8 +224,62 @@ def app(d_vars: dict, parser, config: dict) -> None:
                                 config['CHATID'])
                         else:
                             logging.warning("APITOKEN and/or CHATID not set")
+                    else:
+                        logging.debug("Skipping Telegram alert.")
                     with open(s_telegramcount, 'w') as f:
                         f.write(str(i_telegramcount+1))
+                if config['ENABLE_SLACK'] == '1' and \
+                    d_level[s_level]>=d_level[config['SLACK_ALERT_LEVEL']]:
+                    s_slackcount = f"{s_msgfile_prefix}_{s_level}_" + \
+                                        "slackcount"
+                    if os.path.isfile(s_slackcount):
+                        with open(s_slackcount, 'r') as f:
+                            i_slackcount = int(f.read())
+                    else:
+                        logging.debug(f"Alert level is changed to {s_level}")
+                        logging.debug("Remove other level count files.")
+                        if s_level == 'critical':
+                            l_countfile = [
+                                f"{s_msgfile_prefix}_warning_slackcount",
+                                f"{s_msgfile_prefix}_error_slackcount"
+                            ]
+                        elif s_level == 'error':
+                            l_countfile = [
+                                f"{s_msgfile_prefix}_warning_slackcount",
+                                f"{s_msgfile_prefix}_critical_slackcount"
+                            ]
+                        else:
+                            l_countfile = [
+                                f"{s_msgfile_prefix}_error_slackcount",
+                                f"{s_msgfile_prefix}_critical_slackcount"
+                            ]
+                        for f in l_countfile:
+                            if os.path.isfile(f):
+                                os.unlink(f)
+                        logging.debug(f"Create {s_slackcount}.")
+                        with open(s_slackcount, 'w') as f:
+                            f.write('1')
+                        i_slackcount = 1
+                    if i_slackcount > \
+                        int(config['SLACK_ALERT_COUNT']) + \
+                        int(config['SLACK_ALERT_SKIP']):
+                        logging.debug("Reset the slack alert count.")
+                        with open(s_slackcount, 'w') as f:
+                            f.write('1')
+                        i_slackcount = 1
+                    if i_slackcount <= int(config['SLACK_ALERT_COUNT']):
+                        logging.debug(f"Slack alert: {i_slackcount} " + \
+                                  f"out of {config['SLACK_ALERT_COUNT']}")
+                        logging.debug("Send slack alert.")
+                        if config['SLACK_WEBHOOK']:
+                            s_slack = f"{s_sender}\n{s_subject}\n\n{s_msg}"
+                            send_slack(s_slack, config['SLACK_WEBHOOK'])
+                        else:
+                            logging.warning("SLACK_WEBHOOK is not set")
+                    else:
+                        logging.debug("Skipping Slack alert.")
+                    with open(s_slackcount, 'w') as f:
+                        f.write(str(i_slackcount+1))
             else:
                 s_log = f"The used space of {s_path} ({i_used_percent}%) " + \
                         f"is below warning level ({d_thresholds['warning']}%)"
@@ -237,14 +301,24 @@ def main():
             description='Alert when disk used space is higher than thresholds',
             epilog='Thanks for using %(prog)s! :)',
             formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    #parser.add_argument('-v', '--version', action='version',
+    #        version="{version}".format(version=__version__))
     parser.add_argument('-d', '--debug', action='store_true',
             help='enable debug mode')
     parser.add_argument('-l', '--logfile', help='path to logfile')
-    parser.add_argument('conffile', help='configuration file location')
+    #parser.add_argument('-c', '--conf', 
+    #        default='/etc/diskpatrol.conf',
+    #        help='configuration file location')
+    parser.add_argument('conf', help='configuration file location')
     args = parser.parse_args()
     d_vars = vars(args)
+    if not d_vars['conf'] or not os.path.isfile(d_vars['conf']):
+        s_msg = f"Error: cannot find the conf file {d_vars['conf']}\n"
+        logging.error(s_msg)
+        parser.exit(1, s_msg)
+
     config = {
-        **dotenv_values(d_vars['conffile']),
+        **dotenv_values(d_vars['conf']),
     }
     level = logging.DEBUG if d_vars['debug'] else d_level[config['LOGLEVEL']]
     logfile = d_vars['logfile'] if d_vars['logfile'] else config['LOGFILE']
@@ -256,7 +330,8 @@ def main():
     except Exception as e:
         logging.exception(e)
         parser.exit(1, message=str(e))
-    app(d_vars, parser, config)
+    clean_tmp()
+    app(d_vars, config)
 
 if __name__ == '__main__':
     main()
